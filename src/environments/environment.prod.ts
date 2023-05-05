e4c4ad24f461d74e5b318d63b0b60caf11439cf5
BrayanValdesG <balexis41@gmail.com>
2023-05-05 02:39:35 -0500

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    urlBase: "https://api.themoviedb.org/3"
+    urlBase: "https://api.themoviedb.org/3",
+    apiKey: "d41bffd877802499e927e98410db7f23",
+    language: "es-ES"
 };
